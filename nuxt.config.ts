c2a9a8ed442b1166221aaacb89d92ee6df910547
Edwards <1285704653@qq.com>
2023-08-04 14:33:12 +0800

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@huntersofbook/naive-ui-nuxt',
+    '@unocss/nuxt',
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+        // 自动引入 `defineStore(), storeToRefs()`
+          'defineStore',
+          'storeToRefs',
+        ],
+      },
+    ]],
+  unocss: {
+    uno: true, // enabled `@unocss/preset-uno`
+    icons: true, // enabled `@unocss/preset-icons`
+    attributify: true, // enabled `@unocss/preset-attributify`,
+    // core options
+    shortcuts: [],
+    rules: [],
+    safelist: [],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- <div>
+    <NButton>hello</NButton>
+  </div> -->
+  <div class="i-vscode-icons-file-type-nuxt" />
+  <h1 class="bg-blue-200">
+    Index Page
+  </h1>
+</template>
+
+<style scoped></style>
